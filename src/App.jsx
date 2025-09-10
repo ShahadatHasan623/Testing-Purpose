3a2b1692013239420aff9fb5e53ce81bf07e1ace
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-09-10 21:29:34 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-
   return (
     <>
+      <Navbar></Navbar>
       <h1>hi this is a project for test purpose</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
